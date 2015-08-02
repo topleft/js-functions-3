de077f6cc8444a55583f51590303f19b825d8c06
topleft <pete.james.jeffryes@gmail.com>
2015-08-02 15:51:50 -0600

    finished 6 functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
 // add scripts
 
 console.log("sanity check!");
+
+function getStudentName(obj){
+  return obj.name;
+};
+
+function getTotalLetters(array){
+  var total = 0;
+  array.forEach(function(string){ total += string.length});
+  return total;
+};
+
+function createObject(key, value){
+  return {[key]:value};
+};
+
+function getNegativeIndex(arr, index){
+  var positiveIndex = arr.length + index;
+  return arr[positiveIndex];
+};
+
+function removeCharacter(string, item){
+  return string.split("").map(function(letter){
+    if(letter === item.toString()) letter = "";
+      else return letter;
+    }).join("");
+};
+
+function outputObject(joiner , obj){
+  var str = ""
+  var num = Object.keys(obj).length;
+  for (key in obj) {
+    num--
+    if (num === 0)
+      str += key+" "+joiner+" "+obj[key];
+    else
+      str += key+" "+joiner+" "+obj[key]+", ";
+  }
+  return str
+};
+
+
+
+
+
+
+
+
+
+
+
